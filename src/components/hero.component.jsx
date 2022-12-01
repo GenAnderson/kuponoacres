29601939f45cc878011c14be73a5b60dcd2d84a0
GenAnderson <genchung1@outlook.com>
2022-12-01 11:18:38 -1000

    changed hero bkg to inline for loading and fixed media query at hero again

--- src/components/hero.component.jsx
+++ src/components/hero.component.jsx
@@ -1,16 +1,25 @@
 import "./hero.styles.scss";
 
 import Logo from "../images/logo-nobg.png";
+import backgroundImage from "../images/hero.jpg";
 
 function HeroSection() {
   return (
     <section className="sectionHero">
-      <div className="heroText">
+      <div
+        className="heroText"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: "75vh",
+          width: "100%",
+        }}
+      >
         <h1>Kūpono Acres</h1>
         <div className="heroTextTwo">"Grown with pono"</div>
       </div>
       <img src={Logo} className="kuponoLogo" alt="kupono acres logo" />
-
       <div className="heroTextBelow">
         In the Hawaiian language, the word “Kūpono” means to be forthright,
         fair,and honest in your relationships with others. Loosely translated,
