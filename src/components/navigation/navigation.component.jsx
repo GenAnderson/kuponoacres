f8d4a1ea830acf947fcf61686c8ce138d08013ed
GenAnderson <genchung1@outlook.com>
2022-12-13 11:50:57 -1000

    changed prices and added nav hover descriptions

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -12,51 +12,101 @@ import "./navigation.styles.scss";
 const Navigation = () => {
   const { isCartOpen } = useContext(CartContext);
 
+  const [isHovering1, setIsHovering1] = useState(false);
+  const [isHovering2, setIsHovering2] = useState(false);
+  const [isHovering3, setIsHovering3] = useState(false);
+  const [isHovering4, setIsHovering4] = useState(false);
+
+  const handleMouseOver1 = () => {
+    setIsHovering1(true);
+  };
+  const handleMouseOver2 = () => {
+    setIsHovering2(true);
+  };
+  const handleMouseOver3 = () => {
+    setIsHovering3(true);
+  };
+  const handleMouseOver4 = () => {
+    setIsHovering4(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering1(false);
+    setIsHovering2(false);
+    setIsHovering3(false);
+    setIsHovering4(false);
+  };
+
   return (
     <Fragment>
       <nav>
         <div className="social-media">
-          <a
-            href="https://www.facebook.com/kupono.acres"
-            target="_blank"
-            rel="noreferrer"
+          <div
+            className="nav-icon__Container"
+            onMouseOver={handleMouseOver1}
+            onMouseOut={handleMouseOut}
           >
-            <img
-              src={require("../../images/icon-facebook.png")}
-              className="nav-icons"
-              alt="facebook-icon"
-            />
-          </a>
-
-          <a
-            href="https://www.instagram.com/kuponoacres/?hl=en"
-            target="_blank"
-            rel="noreferrer"
+            <a
+              href="https://www.facebook.com/kupono.acres"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={require("../../images/icon-facebook.png")}
+                className="nav-icons"
+                alt="facebook-icon"
+              />
+            </a>
+            <div className="nav-icon__Bubble">{isHovering1 && <p>FB</p>}</div>
+          </div>
+          <div
+            className="nav-icon__Container"
+            onMouseOver={handleMouseOver2}
+            onMouseOut={handleMouseOut}
           >
-            <img
-              src={require("../../images/icon-instagram.png")}
-              className="nav-icons"
-              alt="instagram-icon"
-            />
-          </a>
-          <Link to="/">
-            <img
-              src={require("../../images/icon-home.png")}
-              className="nav-icons"
-              alt="home-icon"
-            />
-          </Link>
+            <a
+              href="https://www.instagram.com/kuponoacres/?hl=en"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src={require("../../images/icon-instagram.png")}
+                className="nav-icons"
+                alt="instagram-icon"
+              />
+            </a>
+            <div className="nav-icon__Bubble">{isHovering2 && <p>IG</p>}</div>
+          </div>
+          <div
+            className="nav-icon__Container"
+            onMouseOver={handleMouseOver3}
+            onMouseOut={handleMouseOut}
+          >
+            <Link to="/">
+              <img
+                src={require("../../images/icon-home.png")}
+                className="nav-icons"
+                alt="home-icon"
+              />
+            </Link>
+            <div className="nav-icon__Bubble">{isHovering3 && <p>Home</p>}</div>
+          </div>
         </div>
 
         <div className="menu">
-          <Link to="/shop">
-            <img
-              src={require("../../images/icon-shop.png")}
-              className="nav-icons"
-              alt="shop-icon"
-            />
-          </Link>
-
+          <div
+            className="nav-icon__Container"
+            onMouseOver={handleMouseOver4}
+            onMouseOut={handleMouseOut}
+          >
+            <Link to="/shop">
+              <img
+                src={require("../../images/icon-shop.png")}
+                className="nav-icons"
+                alt="shop-icon"
+              />
+            </Link>
+            <div className="nav-icon__Bubble">{isHovering4 && <p>Shop</p>}</div>
+          </div>
           <CartIcon />
         </div>
         {isCartOpen && <CartDropDown />}
